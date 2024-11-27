3c7c356d332c430be3e9abbd8e636bd22cddf651
asmi4 <suman.asmita0408@gmail.com>
2024-11-27 23:33:59 +0530

    Feat: Added code in App.jx

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
+
       {!inputIsValid && (
         <p className="center">
           Please enter duration value greater than or equal to 1
